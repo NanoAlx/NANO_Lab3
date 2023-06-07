aa47535aa1755ff27dfd8ddfa2203eebdfa85aac
NanoAlx <nanoalex24.an@gmail.com>
2023-06-07 16:41:34 -0400

    Lab 3 code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Order order = new Order();
+        order.attach(new PriceObserver());
+        order.attach(new QuantityObserver());
+        order.addItem(250);
+        System.out.println(order);
+        order.addItem(180);
+        System.out.println(order);
     }
 }
